e89f17db2cdb744b38d4209a2ab49a03d85a0756
Yi <438002896@qq.com>
2017-04-18 15:12:00 +0800

    de

--- static/js/heart.js
+++
@@ -1,8 +0,0 @@
-window.onload = function(){
-	var heartcontent = document.getElementById('heartcontent');
-	document.getElementById('editbt').addEventListener('click' , function(){
-		if (heartcontent.value.trim() != ""){
-			document.getElementById('editheart').submit();
-		}
-	});
-};
